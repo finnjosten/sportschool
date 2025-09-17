daf54771736736f07b564eefa5cf9a89162bf8bc
Michusow <1194779@student.roc-nijmegen.nl>
2025-09-17 12:28:42 +0200

    accountbeheer + path (not working correctly due to merge conflicts)

---
+++ kastoe/src/account_beheer/accountbeheer.jsx
@@ -0,0 +1,28 @@
+import "./accountbeheer.css";
+
+function Rectangle({ title, boxes }) {
+  return (
+    <div className="rectangle">
+      <div className="rectangle-title">{title}</div>
+      <div className="box-row">
+        {boxes.map((box, idx) => (
+          <div className="box" key={idx}>{box}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function AccountBeheer() {
+  return (
+    <div className="login-page-background">
+      <div className="rectangle-row">
+        <Rectangle  boxes={["Box 1", "Box 2", "Box 3"]} />
+        <Rectangle  boxes={["Box 1", "Box 2", "Box 3"]} />
+        <Rectangle  boxes={["Box 1", "Box 2", "Box 3"]} />
+      </div>  
+    </div>
+  );
+}
+
+export default AccountBeheer;
--- kastoe/src/main.jsx
+++ kastoe/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import LoginPage from './login-page/LoginPage.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoginPage /> {/* Update this component */}
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
