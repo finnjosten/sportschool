0bb87cdf7309d50ff377b91bbeee5a0e00081418
Finn Josten <finn.r.josten@proton.me>
2025-09-18 09:25:41 +0200

    cards

--- kastoe/src/accountPage/accountPage.jsx
+++ kastoe/src/accountPage/accountPage.jsx
@@ -1,12 +1,38 @@
 import "./accountPage.css";
 
-function Rectangle({ title, boxes }) {
+function CursusCard({ title, items }) {
     return (
-        <div className="wpb-card wpb-card--col">
+        <div className="wpb-card wpb-card--col wpb-card--col-cursus">
             <h3 className="wpb-card__title">{title}</h3>
             <div className="wpb-card__boxes">
-                {boxes?.map((box, idx) => (
-                    <div className="wpb-card wpb-card--box" key={idx}>{box}</div>
+                {items?.map((item, idx) => (
+                    <div className="wpb-card wpb-card--box" key={idx}>{item.title}</div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function TrainerCard({ title, items }) {
+    return (
+        <div className="wpb-card wpb-card--col wpb-card--col-trainer">
+            <h3 className="wpb-card__title">{title}</h3>
+            <div className="wpb-card__boxes">
+                {items?.map((item, idx) => (
+                    <div className="wpb-card wpb-card--box" key={idx}>{item.title}</div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function AccountCard({ title, items }) {
+    return (
+        <div className="wpb-card wpb-card--col wpb-card--col-trainer">
+            <h3 className="wpb-card__title">{title}</h3>
+            <div className="wpb-card__boxes">
+                {items?.map((item, idx) => (
+                    <div className="wpb-card wpb-card--box" key={idx}>{item.title}</div>
                 ))}
             </div>
         </div>
@@ -14,13 +40,96 @@ function Rectangle({ title, boxes }) {
 }
 
 function AccountPage() {
+    const cursusOptions = [
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+        {
+            title: "Yoga cursus",
+            description: "Woensdag 19 januari is er een yoga cursus om 21:00"
+        },
+    ];
+    const trainerOptions = [
+        {
+            title: "John doe",
+            description: "Personal trainer specialized in body building"
+        },
+        {
+            title: "John doe",
+            description: "Personal trainer specialized in body building"
+        },
+        {
+            title: "John doe",
+            description: "Personal trainer specialized in body building"
+        },
+        {
+            title: "John doe",
+            description: "Personal trainer specialized in body building"
+        },
+    ];
+    const accountOptions = [
+        {
+            title: "Verhoog abonnement",
+            description: "Verhoog je abonnement naar niveau {niveau}"
+        },
+        {
+            title: "Verlaag abonnement",
+            description: "Verlaag je abonnement naar niveau {niveau}"
+        },
+        {
+            title: "Abonnement annuleren",
+            description: "Annuleer makkelijk je abonnement met de knop"
+        },
+        {
+            title: "Uitloggen",
+            description: "Loguit van je account beheer"
+        }
+    ];
+
     return (
         <div className="account-page-background">
             <div className="container">
                 <div className="inner">
-                    <Rectangle title="Aanmelden voor cursus" boxes={["Workout 1", "Workout 2", "Workout 3"]} />
-                    <Rectangle title="Contact met trainer" boxes={["Voeding 1", "Voeding 2", "Voeding 3"]} />
-                    <Rectangle title="Abonnement beheer" boxes={["Progressie 1", "Progressie 2", "Progressie 3"]} />
+                    <CursusCard title="Aanmelden voor cursus" items={cursusOptions} />
+                    <TrainerCard title="Contact met trainer" items={trainerOptions} />
+                    <AccountCard title="Abonnement beheer" items={accountOptions} />
                 </div>
             </div>
         </div>
