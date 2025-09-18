7d761a81be5c1609ce1205e2f3fc193c3dea033d
Finn Josten <finn.r.josten@proton.me>
2025-09-18 09:08:42 +0200

    account page styling

--- kastoe/src/accountPage/accountPage.jsx
+++ kastoe/src/accountPage/accountPage.jsx
@@ -2,11 +2,11 @@ import "./accountPage.css";
 
 function Rectangle({ title, boxes }) {
     return (
-        <div className="rectangle">
-            <div className="rectangle-title">{title}</div>
-            <div className="box-row">
-                {boxes.map((box, idx) => (
-                    <div className="box" key={idx}>{box}</div>
+        <div className="wpb-card wpb-card--col">
+            <h3 className="wpb-card__title">{title}</h3>
+            <div className="wpb-card__boxes">
+                {boxes?.map((box, idx) => (
+                    <div className="wpb-card wpb-card--box" key={idx}>{box}</div>
                 ))}
             </div>
         </div>
@@ -15,11 +15,13 @@ function Rectangle({ title, boxes }) {
 
 function AccountPage() {
     return (
-        <div className="login-page-background">
-            <div className="rectangle-row">
-                <Rectangle boxes={["Box 1", "Box 2", "Box 3"]} />
-                <Rectangle boxes={["Box 1", "Box 2", "Box 3"]} />
-                <Rectangle boxes={["Box 1", "Box 2", "Box 3"]} />
+        <div className="account-page-background">
+            <div className="container">
+                <div className="inner">
+                    <Rectangle title="Aanmelden voor cursus" boxes={["Workout 1", "Workout 2", "Workout 3"]} />
+                    <Rectangle title="Contact met trainer" boxes={["Voeding 1", "Voeding 2", "Voeding 3"]} />
+                    <Rectangle title="Abonnement beheer" boxes={["Progressie 1", "Progressie 2", "Progressie 3"]} />
+                </div>
             </div>
         </div>
     );
